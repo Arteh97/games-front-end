53af7d22b22da8094c5d2e85e7b16a153fb085a1
Mahamud Arteh <artehrush@gmail.com>
2021-08-31 21:49:39 +0100

    Categories Page Fully Functional, Styling Left to do

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from "react";
-// import axios from "axios";
+import axios from "axios";
 import styles from "./Categories.module.css";
-// import Logo from "../../elements/Logo/Logo";
+import Logo from "../../elements/Logo/Logo";
 import icon from "../../images/categories-icon.png";
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
-  // const [slugInput, setSlugInput] = useState("");
-  // const [descriptionInput, setDescriptionInput] = useState("");
+  const [slugInput, setSlugInput] = useState("");
+  const [descriptionInput, setDescriptionInput] = useState("");
 
-  // const handleSlugChange = (e) => {
-  //   setSlugInput(e.currentTarget.value);
-  // };
-  // const handleDescriptionChange = (e) => {
-  //   setDescriptionInput(e.currentTarget.value);
-  // };
+  const handleSlugChange = (e) => {
+    setSlugInput(e.currentTarget.value);
+  };
+  const handleDescriptionChange = (e) => {
+    setDescriptionInput(e.currentTarget.value);
+  };
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   const newCategory = {};
-  //   newCategory.slug = slugInput;
-  //   newCategory.description = descriptionInput;
-  //   axios.post("https://arteh97.herokuapp.com/api/categories", newCategory);
-  // };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newCategory = {};
+    newCategory.slug = slugInput;
+    newCategory.description = descriptionInput;
+    axios.post("https://arteh97.herokuapp.com/api/categories", newCategory);
+  };
 
   useEffect(() => {
     fetch("https://arteh97.herokuapp.com/api/categories")
@@ -35,9 +35,36 @@ const Categories = () => {
 
   return (
     <div className={styles.container}>
-      <header>Categories</header>
-      {/* <div className={styles.__categories}> */}
       <section className="card-group">
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <h2>Post Category</h2>
+          <label className="form">
+            Category:
+            <textarea
+              onChange={handleSlugChange}
+              rows="1"
+              value={slugInput}
+              type="text"
+              placeholder="new category here..."
+            />
+          </label>
+          <label type="submit" className="">
+            Description
+            <textarea
+              onChange={handleDescriptionChange}
+              rows="1"
+              placeholder="description goes here..."
+            />
+          </label>
+          <button
+            className={styles.form__button}
+            type="submit"
+            onClick={handleSubmit}
+          >
+            submit
+          </button>
+        </form>
+
         {categories.map((category) => {
           return (
             <article key={category.slug}>
@@ -54,7 +81,6 @@ const Categories = () => {
           );
         })}
       </section>
-      {/* </div> */}
     </div>
   );
 };
